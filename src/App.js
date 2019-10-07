c86322fba4131bb46cb9a6afe7cfa6a0e04e7a6a
saung3754 <saung3754@nntuple.com>
2019-10-07 23:16:29 +0900

    propTypes check

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 const foodName =[
   {
     id:1,
     name: "김치",
-    img:"http://m.08food.com/web/product/big/201801/32_shop1_475359.jpg"
+    img:"http://m.08food.com/web/product/big/201801/32_shop1_475359.jpg",
+    rating:5
   },
   {
     id:2,
     name:"떡볶이",
-    img:"https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/55oP/image/hpJrhAab-Ph2aW_vxpH-c5CCYto.jpg"
+    img:"https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/55oP/image/hpJrhAab-Ph2aW_vxpH-c5CCYto.jpg",
+    rating:4.8
   },
   {
     id:3,
     name: "순대",
-    img:"http://sanghafarm.co.kr/sanghafarm_Data/upload/shop/product/201810/A0003528_2018102319172333992.jpg"
+    img:"http://sanghafarm.co.kr/sanghafarm_Data/upload/shop/product/201810/A0003528_2018102319172333992.jpg",
+    rating:4.5
   },
   {
     id:4,
     name: "케익",
-    img:"http://m.ops.co.kr/web/product/big/201806/87_shop1_15296451981396.jpg"
+    img:"http://m.ops.co.kr/web/product/big/201806/87_shop1_15296451981396.jpg",
+    rating:4.2
   },
   {
     id:5,
     name: "아이스아메리카노",
-    img:"https://img.seoul.co.kr/img/upload/2019/02/13/SSI_20190213100441_V.jpg"
+    img:"https://img.seoul.co.kr/img/upload/2019/02/13/SSI_20190213100441_V.jpg",
+    rating:5
   }
 ];
 
-function Test ({name,img}){
+function Test ({name,img,rating}){
   return (
   <div>
     <h2>I like{name} </h2>
+    <h4>{rating}/5.0</h4>
     <img src={img} alt={name}/>
   </div>
   );
 }
 
-function RenderFood(dish){
-  return <Test key ={dish.id} name= {dish.name} img={dish.img}/>
+Test.propTypes ={
+  name:propTypes.string.isRequired,
+  img: propTypes.string.isRequired,
+  rating:propTypes.number.isRequired
 }
+
 
 function App() {
   return (
     <div className="App">
-      {/* 바로 넣는 방법 */}
       {foodName.map(curFood=>(
-        <Test key={curFood.id} name={curFood.name} img={curFood.img}/>
+        <Test key={curFood.id} name={curFood.name} img={curFood.img} rating= {curFood.rating}/>
       ))}
-      {/* 함수를 이용해 넣는 방법 */}
-      {console.log(foodName.map(RenderFood))}
-      {foodName.map(RenderFood)}
     </div>
   );
 }
