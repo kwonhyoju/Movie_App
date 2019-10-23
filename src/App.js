74a36d857dfd1361b14017dcb2148edaed54ea28
saung3754 <saung3754@nntuple.com>
2019-10-23 23:19:50 +0900

    gh-pages 경로 오류 수정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Movies from './Movies.js'
 import './App.css';
+//  build 후 gh-pages 안되서 dev-tool에서 network확인 후 responce안오는 링크들 build된 폴더로 가서 링크 연결 다시함
 
 class App extends React.Component{
     state ={
